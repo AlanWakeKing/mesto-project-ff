bac452aebca3e536b6fac165ed5985e671bec2f6
Aleksey Kirsanov <lexamenz@ya.ru>
2024-05-14 13:48:25 +0300

    #0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
+const cardsContent = document.querySelector('.places__list');
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(cardData,deleteCard){
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
+	const cardImg = cardElement.querySelector('.card__image');
+  cardImg.src = cardData.link;
+  cardImg.alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+	cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+	return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+  cardElement.remove(); 
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(card => {
+	const cardElement = createCard(card,deleteCard);
+	cardsContent.append(cardElement);
+}
+);
